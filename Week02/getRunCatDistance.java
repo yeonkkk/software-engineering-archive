8cf7399cb45ff9c4e0d532342908f8bdc6b04861
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-04 17:12:46 +0900

    Create getRunCatDistance.java

---
+++ Week02/getRunCatDistance.java
@@ -0,0 +1,9 @@
+package com.codestates.coplit; 
+
+public class Solution { 
+	public int getRunCatDistance(int speed, int time) {
+  	int distance;
+    distance = speed * time;
+  	return distance;
+	}
+}
