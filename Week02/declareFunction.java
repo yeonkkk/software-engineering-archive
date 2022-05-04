e7b030d8dfa0d3ecc9f18b039e7b08475803eb58
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-04 17:13:18 +0900

    Create declareFunction.java

---
+++ Week02/declareFunction.java
@@ -0,0 +1,13 @@
+package com.codestates.coplit; 
+
+public class Solution { 
+	public int multipleBy2(int input) {
+    input = input * 2;
+    return input;
+  }
+
+  public int devideBy2(int input) {
+    input = input / 2;
+    return input;
+  }
+}
