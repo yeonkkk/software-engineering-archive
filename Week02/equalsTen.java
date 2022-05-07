b09b8a93950df07f6a3bce97f4fe763cb1bb6266
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-07 21:15:11 +0900

    Create equalsTen.java

---
+++ Week02/equalsTen.java
@@ -0,0 +1,13 @@
+package com.codestates.coplit; 
+
+public class Solution { 
+	public boolean equalsTen(int num) {
+    // TODO:
+    switch (num) {
+      case 10:
+        return true;
+      default:
+        return false;
+    }
+	} 
+}
