fe853eb40c8f6c5c8985ccc9c7d81f6941d99b3d
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-07 21:20:03 +0900

    Create addOneSecond.java

---
+++ Week02/addOneSecond.java
@@ -0,0 +1,20 @@
+package com.codestates.coplit; 
+
+public class Solution { 
+	public String addOneSecond(int hour, int minute, int second) {
+    // TODO:
+    second++;
+    if (second >= 60) {
+      second = 0;
+      minute++;
+      if (minute >= 60) {
+        minute = 0;
+        hour++;
+        if (hour == 24) {
+          hour = 0;
+        } 
+      }
+    }
+    return String.format("1초 뒤에 %d시 %d분 %d초 입니다", hour, minute, second);
+	} 
+}
