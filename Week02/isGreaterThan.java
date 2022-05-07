6ab9a480164d43dec35d9ebcb1a6f0277cefa677
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-07 21:15:32 +0900

    Create isGreaterThan.java

---
+++ Week02/isGreaterThan.java
@@ -0,0 +1,12 @@
+package com.codestates.coplit; 
+
+public class Solution { 
+	public boolean isGreaterThan(int num1, int num2) {
+    // TODO: 
+    if (num1 <num2) {
+      return true;
+    } else {
+      return false;
+    }
+	} 
+}
