3e3a2fd6ea19cd9a7430b55bc343f2ce54a1868b
김성연 <yeonkkk@gimseong-yeon-ui-MacBookPro.local>
2022-05-09 12:09:19 +0900

    과제

--- src/com/codestates/seb/calculator/Calculator.java
+++ src/com/codestates/seb/calculator/Calculator.java
@@ -6,6 +6,34 @@ public class Calculator {
     System.out.println("===Java Calculator===");
 
     Scanner input = new Scanner(System.in);
+
+    while (true) {
+      double firstNum = input.nextDouble();
+      char operator = input.next().charAt(0);
+      double secondNum = input.nextDouble();
+
+      switch (operator) {
+        case '+':
+          System.out.println(firstNum + secondNum);
+          break;
+        case '-':
+          System.out.println(firstNum - secondNum);
+          break;
+        case '*':
+          System.out.println(firstNum * secondNum);
+          break;
+        case '/':
+          System.out.println(firstNum / secondNum);
+          break;
+        default:
+          System.out.println("잘못된 입력입니다");
+          break;
+      }
+    }
+  }
+}
+
+
         /*
             요구 사항에 따라 간단한 계산기를 만들어주세요.
             1. 사용자의 입력으로 첫 번째 숫자, 연산자, 두 번째 숫자를 받아야 합니다.
@@ -13,6 +41,3 @@ public class Calculator {
             3. 소수점 연산을 수행할 수 있어야 합니다.
             4. 연산 결과를 콘솔에 출력합니다.
         */
-    // TODO...
-  }
-}
