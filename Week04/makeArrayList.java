72866874359534018a69b9c64f03b35cf03ba4ba
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-20 16:44:52 +0900

    Create makeArrayList.java

---
+++ Week04/makeArrayList.java
@@ -0,0 +1,12 @@
+package com.codestates.coplit; 
+import java.util.*;
+
+public class Solution { 
+  public ArrayList<Integer> makeArrayList() {
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    for (int i=1; i<6; i++){
+      arrayList.add(i);
+      }
+    return arrayList;
+  }
+}
