2ef7e4513ea00a00da3062f883d3f7a36dc6963d
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-20 16:54:53 +0900

    Create removeEntry.java

---
+++ Week04/removeEntry.java
@@ -0,0 +1,8 @@
+package com.codestates.coplit; 
+import java.util.*;
+
+public class Solution { 
+  public void removeEntry(HashMap<String, Integer> hashMap, String key) {
+    hashMap.remove(key);
+  }
+}
