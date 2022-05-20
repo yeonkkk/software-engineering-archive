ba70711abd87b792143d15bc372fef3a8fd63870
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-05-20 16:56:10 +0900

    Create isContain.java

---
+++ Week04/isContain.java
@@ -0,0 +1,8 @@
+package com.codestates.coplit; 
+import java.util.*;
+
+public class Solution { 
+  public boolean isContain(HashMap<String, Integer> hashMap, String key) {
+    return hashMap.containsKey(key);
+  }
+}
