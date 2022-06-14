355629a17244c33d3bcf8cf4e7ff48057f20c793
Seongyeon Kim <88660886+yeonkkk@users.noreply.github.com>
2022-06-14 09:36:49 +0900

    Update superIncreasing.java

--- Week08/daily/superIncreasing.java
+++ Week08/daily/superIncreasing.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Solution { 
-	public boolean superIncreasing(int[] arr) {
-    int sum = arr[0];
+    public boolean superIncreasing(int[] arr) {
+        int sum = arr[0];
 
-    for (int i=1; i<arr.length; i++) {
-      if (sum >= arr[i]) return false;
-      else sum += arr[i];
-    }
-    return true;
-	} 
+    	for (int i=1; i<arr.length; i++) {
+            if (sum >= arr[i]) return false;
+      	    else sum += arr[i];
+        }
+        return true;
+    } 
 }
