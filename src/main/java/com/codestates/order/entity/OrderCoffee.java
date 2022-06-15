be18b8a2bcc25b3c2c1fe25fce05d81835018610
hjs6877 <hjs6877@gmail.com>
2022-06-15 10:44:35 +0900

    template tx

---
+++ src/main/java/com/codestates/order/entity/OrderCoffee.java
@@ -0,0 +1,51 @@
+package com.codestates.order.entity;
+
+import com.codestates.audit.Auditable;
+import com.codestates.coffee.entity.Coffee;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class OrderCoffee extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderCoffeeId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "COFFEE_ID")
+    private Coffee coffee;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    public void addOrder(Order order) {
+        this.order = order;
+        if (!this.order.getOrderCoffees().contains(this)) {
+            this.order.getOrderCoffees().add(this);
+        }
+    }
+
+    public void addCoffee(Coffee coffee) {
+        this.coffee = coffee;
+        if (!this.coffee.getOrderCoffees().contains(this)) {
+            this.coffee.addOrderCoffee(this);
+        }
+    }
+}
---
+++ src/main/java/com/codestates/stamp/Stamp.java
@@ -0,0 +1,39 @@
+package com.codestates.stamp;
+
+import com.codestates.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Stamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long stampId;
+
+    @Column(nullable = false)
+    private int stampCount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+        if (member.getStamp() != this) {
+            member.setStamp(this);
+        }
+    }
+}
