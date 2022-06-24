0c215959856320430641851a74b1d9a01e6e8828
김성연 <tjddus0302@gmail.com>
2022-06-24 18:22:22 +0900

    과제

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -1,44 +1,37 @@
 package com.codestates.coffee;
 
+import com.codestates.member.MemberPostDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/coffees")
+@Validated
 public class CoffeeController {
     // 1. DTO 클래스 및 유효성 검증을 적용하세요.
     @PostMapping
-    public ResponseEntity postCoffee(@RequestParam("korName") String korName,
-                                     @RequestParam("engName") String engName,
-                                     @RequestParam("price") int price) {
-        Map<String, Object> map = new HashMap<>();
-        map.put("korName", korName);
-        map.put("engName", engName);
-        map.put("price", price);
-
-
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    public ResponseEntity postCoffee(@Valid @RequestBody CoffeePostDto coffeePostDto) {
+        return new ResponseEntity<>(coffeePostDto, HttpStatus.CREATED);
     }
 
     // 2. DTO 클래스 및 유효성 검증을 적용하세요.
     @PatchMapping("/{coffee-id}")
-    public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
-                                      @RequestParam("korName") String korName,
-                                      @RequestParam("price") int price) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("coffeeId", coffeeId);
-        body.put("korName", korName);
-        body.put("engName", "Vanilla Latte");
-        body.put("price", price);
-        return new ResponseEntity<>(body, HttpStatus.OK);
+    public ResponseEntity patchCoffee(@PathVariable("coffee-id") @Min(1) long coffeeId,
+                                      @Valid @RequestBody CoffeePatchDto coffeePatchDto) {
+        coffeePatchDto.setCoffeeId(coffeeId);
+
+        return new ResponseEntity<>(coffeePatchDto, HttpStatus.OK);
     }
 
     @GetMapping("/{coffee-id}")
-    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") @Min(1) long coffeeId) {
         System.out.println("# coffeeId: " + coffeeId);
 
         // not implementation
@@ -56,7 +49,7 @@ public class CoffeeController {
     }
 
     @DeleteMapping("/{coffee-id}")
-    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
+    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") @Min(1) long coffeeId) {
         // No need business logic
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
