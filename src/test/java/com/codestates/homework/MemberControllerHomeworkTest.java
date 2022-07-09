0350dff733abaad5482444e73a87a513d8a773f8
hjs6877 <hjs6877@gmail.com>
2022-07-09 11:29:18 +0900

    Homework Mocking Test

---
+++ src/test/java/com/codestates/homework/MemberControllerHomeworkTest.java
@@ -0,0 +1,50 @@
+package com.codestates.homework;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerHomeworkTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Gson gson;
+
+    @Test
+    void postMemberTest() throws Exception {
+        // TODO MemberController의 postMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
+        // TODO Mockito를 사용해야 합니다. ^^
+    }
+
+    @Test
+    void patchMemberTest() throws Exception {
+        // TODO MemberController의 patchMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
+        // TODO Mockito를 사용해야 합니다. ^^
+    }
+
+    @Test
+    void getMemberTest() throws Exception {
+        // TODO MemberController의 getMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
+        // TODO Mockito를 사용해야 합니다. ^^
+    }
+
+    @Test
+    void getMembersTest() throws Exception {
+        // TODO MemberController의 getMembers() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
+        // TODO Mockito를 사용해야 합니다. ^^
+    }
+
+    @Test
+    void deleteMemberTest() throws Exception {
+        // TODO MemberController의 deleteMember() 핸들러 메서드를 테스트하는 테스트 케이스를 여기에 작성하세요.
+        // TODO Mockito를 사용해야 합니다. ^^
+    }
+}
