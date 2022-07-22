3e97c031aee3b9989f33f72b85df6a81b249052a
yeonkkk <tjddus0302@gmail.com>
2022-07-22 16:26:10 +0900

    필터 실습

---
+++ Week13/spring-security/practice/src/main/java/com/yeonkkk/filter/SecondFilter.java
@@ -0,0 +1,26 @@
+package com.yeonkkk.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class SecondFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        System.out.println("SecondFilter가 생성되었습니다.");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("==========Second 필터 시작==========");
+        chain.doFilter(request, response);
+        System.out.println("==========Second 필터 종료==========");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("SecondFilter가 사라집니다.");
+        Filter.super.destroy();
+    }
+}
